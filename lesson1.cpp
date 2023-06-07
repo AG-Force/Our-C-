a37fd21d0617b35ec32ad75347f10674ff5ee4ec
AG-Force <randa712807@icloud.com>
2023-06-07 17:43:01 +0200

    first commit

---
+++ lesson1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    std::cout << "Hallo, Welt!" << std::endl;
+    return 0;
+}
+
+
+/*
+Das Grundgerüst
+#include <iostream>
+using namespace std;
+int main() {
+    return 0;
+}
+
+*/
