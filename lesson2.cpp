4220c066684809dcc9ace9546b95ba0b90c89a72
AG-Force <randa712807@icloud.com>
2023-06-08 14:54:51 +0200

    cout get know

---
+++ lesson2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  // Include the iostream library for input/output operations
+
+using namespace std;  // Use the std namespace
+
+int main() {
+    cout << "hello" << endl;  // Print "hello" and move to the next line
+    cout << "hi" << endl;  // Print "hi" and move to the next line
+    cout << "hello" << endl << "test" << endl;  // Print "hello", move to the next line, print "test", and move to the next line
+    cout << 4;  // Print the number 4
+    cout << 4;  // Print the number 4
+    cout << 4 << endl;  // Print the number 4 and move to the next line
+    cout << 4;  // Print the number 4
+
+    return 0;  // Exit the program with a status code of 0
+}
