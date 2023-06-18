3f1b25d4c8f76db87c926cbdf4e5a46d1ec498f6
AG-Force <randa712807@icloud.com>
2023-06-18 20:07:05 +0200

    variables

---
+++ lesson4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    x = 10;
+    cout << x << endl;
+
+    int z;
+    z = 20;
+    cout << z << endl;
+
+    int e, y; // Namen von Variablen geändert
+    e = 23, y = 22;
+    cout << e<<endl<<y;
+
+    return 0;
+}
