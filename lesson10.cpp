903db9686f3e5aa3f213a1298599cfb05324a7ee
AG-Force <randa712807@icloud.com>
2023-06-24 19:26:08 +0200

    area triangle

---
+++ lesson10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int base = 5, height = 9, area;
+    
+    // Calculating the area of the triangle using the correct formula
+    area = 0.5 * base * height;
+    
+    // Printing the value of 'area' along with a descriptive message
+    cout << "Area of triangle is = " << area << endl;
+//////////////////////////////////////////////////////////////
+    int base2 = 5, height2 = 9;
+    float area2;
+    // Calculating the area of the triangle using the correct formula
+    area2 = 0.5 * base2 * height2;
+    
+    // Printing the value of 'area' along with a descriptive message
+    cout << "Area of triangle is = " << area2 << endl;
+    return 0;
+}
