4b3932ca54afa70bf1ace083424b0bb90f2cff9b
AG-Force <randa712807@icloud.com>
2023-06-24 19:03:45 +0200

    mathe

---
+++ lesson6.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Printing the result of 7 + 5
+    cout << 7 + 5 << endl;
+
+    // Printing the string "7+5"
+    cout << "7+5" << endl;
+
+    // Printing the result of 7 * 5
+    cout << 7 * 5 << endl;
+
+    //////////////////////////////////////////////
+
+    int aa = 9;
+    int bb = 16;
+
+    // Printing the result of aa + bb
+    cout << aa + bb << endl;
+
+    //////////////////////////////////////////////
+
+    int aa1 = 3, bb1 = 17;
+
+    // Printing the result of aa1 + bb1
+    cout << aa1 + bb1 << endl;
+
+    //////////////////////////////////////////////
+
+    // Printing the result of aa1 - bb1
+    cout << aa1 - bb1 << endl;
+
+    //////////////////////////////////////////////
+
+    int aa2 = 12, bb2 = 8;
+
+    // Calculating the sum of aa2 and bb2 and storing it in the 'sum' variable
+    int sum = aa2 + bb2;
+
+    // Printing the value of 'sum'
+    cout << sum << endl;
+
+    return 0;
+}
