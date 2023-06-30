2ce5a7663162345b5b8383587cdca62fe53baf9b
AG-Force <randa712807@icloud.com>
2023-06-30 10:37:32 +0200

    average

---
+++ lesson11.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    This code block is commented out.
+    int num1, num2, num3;
+    float average;
+    
+    num1 = 22;
+    num2 = 23;
+    num3 = 25;
+    average = (num1 + num2 + num3) /3.0;
+
+    cout << "average = " << average << endl;
+    */
+    
+    // Uncommented code starts here
+    
+    float num1, num2, num3; // Declaring three float variables: num1, num2, and num3
+    float average; // Declaring a float variable: average
+    
+    num1 = 22; // Assigning the value 22 to num1
+    num2 = 23; // Assigning the value 23 to num2
+    num3 = 25; // Assigning the value 25 to num3
+    average = (num1 + num2 + num3) / 3; // Calculating the average of num1, num2, and num3
+    
+    cout << "average = " << average << endl; // Printing the average value
+    
+    return 0;
+}
