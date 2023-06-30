073df2bb2f6395f7524879dfb9eb845743d3b13c
AG-Force <randa712807@icloud.com>
2023-06-30 10:38:39 +0200

    data types

---
+++ lesson12.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a1 = 2; // Declaring an integer variable: a1 and assigning the value 2 to it
+    float a2 = 3; // Declaring a float variable: a2 and assigning the value 3 to it
+    double a3 = 4; // Declaring a double variable: a3 and assigning the value 4 to it
+    char a4 = '2'; // Declaring a character variable: a4 and assigning the value '2' to it
+    bool a5 = false; // Declaring a boolean variable: a5 and assigning the value false to it
+    string a6 = "dfsdfsdfsdfsdf"; // Declaring a string variable: a6 and assigning the value "dfsdfsdfsdfsdf" to it
+
+    return 0;
+}
